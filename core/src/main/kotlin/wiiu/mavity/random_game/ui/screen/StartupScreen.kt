37b3d5987f40890462438a12b260fa524960eeb4
AngerVon2468 <thomaschad0909@gmail.com>
2025-06-21 17:52:16 +1000

    Update the StartupScreen to use the looping abstraction.

--- core/src/main/kotlin/wiiu/mavity/random_game/ui/screen/StartupScreen.kt
+++ core/src/main/kotlin/wiiu/mavity/random_game/ui/screen/StartupScreen.kt
@@ -6,35 +6,25 @@ import wiiu.mavity.random_game.Main
 import ktx.app.KtxScreen
 
 import wiiu.mavity.random_game.ui.UIScreen
-import wiiu.mavity.random_game.util.nanoTime
 
 class StartupScreen : KtxScreen, UIScreen {
-
-	var startTime: Long = 0
-	var resetCounter: Boolean = true
 
 	val textRenderer: CrawlText = CentredCrawlTextRenderer(Main.font, "Mavity Presents:")
 
 	override fun render(delta: Float) {
 		this.textRenderer.draw(Main.batch)
-		if (!this.textRenderer.completed)  return
-		val nanoTime = nanoTime
-		if (resetCounter) {
-			startTime = nanoTime
-			resetCounter = false
-			return
-		}
-		if (nanoTime - startTime <= 2_500_000_000) return
-		when (this.textRenderer.text) {
-			"<TITLECARD>" -> {
-				Main.screen = LoadingScreen()
-				dispose()
+		this.textRenderer.loop(
+			condition = { this.completed },
+			targetWait = 2_500_000_000,
+			onComplete = {
+				when (this@StartupScreen.textRenderer.text) {
+					"<TITLECARD>" -> {
+						Main.screen = LoadingScreen()
+						this@StartupScreen.dispose()
+					}
+					else -> this@StartupScreen.textRenderer.newText("<TITLECARD>")
+				}
 			}
-			else -> {
-				this.textRenderer.newText("<TITLECARD>")
-				startTime = 0
-				resetCounter = true
-			}
-		}
+		)
 	}
 }
