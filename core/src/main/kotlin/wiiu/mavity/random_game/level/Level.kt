98f2ab5fa70e7ba70a2f2cd1d03b3ae7c5bdd1a3
AngerVon2468 <thomaschad0909@gmail.com>
2025-07-27 19:43:13 +1000

    Soon:tm:

--- core/src/main/kotlin/wiiu/mavity/random_game/level/Level.kt
+++ core/src/main/kotlin/wiiu/mavity/random_game/level/Level.kt
@@ -3,23 +3,71 @@ package wiiu.mavity.random_game.level
 import com.google.gson.*
 import com.google.gson.stream.*
 
-data class Position(val first: Int, val second: Int) {
+import wiiu.mavity.random_game.world.entity.Entity
+
+// TODO: Switch Chunk#tiles to Map<Position, TileInfo>. Move Two to Utils?
+
+open class Two<out A, out B>(open val first: A, open val second: B) {
+
+	constructor(pair: Pair<A, B>) : this(pair.first, pair.second)
 
 	override fun toString(): String = "$first:$second"
 
+	override fun equals(other: Any?): Boolean {
+		if (other === this) return true
+		if (other !is Two<*, *>) return false
+		if (other.first != this.first) return false
+		if (other.second != this.second) return false
+		if (other.hashCode() != this.hashCode()) return false
+		return true
+	}
+
+	override fun hashCode(): Int {
+		var result = this.first?.hashCode() ?: 0
+		result = 31 * result + (this.second?.hashCode() ?: 0)
+		return result
+	}
+
 	companion object {
 
-		fun fromString(str: String): Position {
+		inline fun <reified A, reified B> fromString0(
+			str: String,
+			convert1: (String) -> A,
+			convert2: (String) -> B
+		): Two<A, B> {
 			val rawPos = str.split(":")
-			return Position(rawPos[0].toInt(), rawPos[1].toInt())
+			return Two(convert1(rawPos[0]), convert2(rawPos[1]))
 		}
+
+		inline fun <reified T> fromString0(
+			str: String,
+			convert: (String) -> T
+		): Two<T, T> = fromString0(str, convert, convert)
 	}
 }
+
+typealias Position = Two<Int, Int>
+val Position.x: Int get() = this.first
+val Position.y: Int get() = this.second
+fun Two.Companion.fromStringP(str: String): Position = fromString0(str) { it.toInt() }
+
+// If you have IDE warnings about the 'ID' property name,
+// change IntelliJ's inspector to check for valid properties
+// using this regex instead: '([a-z]|ID)[A-Za-z\d]*'
+
+typealias TileID = Two<String, String>
+val TileID.declarer: String get() = this.first
+val TileID.ID: String get() = this.first
+fun Two.Companion.fromStringTI(str: String): TileID = fromString0(str) { it }
+
+typealias TileInfo = Two<TileID, Class<out Entity>?>
+val TileInfo.ID: TileID get() = this.first
+val TileInfo.entityClass: Class<out Entity>? get() = this.second
 
 data class Level(val chunks: Map<Position, Chunk>)
 
 // tiles: Position : Tile ID
-data class Chunk(val position: Position, val tiles: Map<Position, String>)
+data class Chunk(val position: Position, val tiles: Map<Position, TileID>)
 
 object LevelTypeAdapter : TypeAdapter<Level>() {
 
@@ -54,18 +102,18 @@ object ChunkTypeAdapter : TypeAdapter<Chunk>() {
 
 		out.beginObject()
 
-		for (entry in value.tiles) out.name(entry.key.toString()).value(entry.value)
+		for (entry in value.tiles) out.name(entry.key.toString()).value(entry.value.toString())
 
 		out.endObject()
 	}
 
 	override fun read(`in`: JsonReader): Chunk {
-		val position = Position.fromString(`in`.nextName())
-		val tiles: MutableMap<Position, String> = mutableMapOf()
+		val position = Position.fromStringP(`in`.nextName())
+		val tiles: MutableMap<Position, TileID> = mutableMapOf()
 
 		`in`.beginObject()
 
-		while (`in`.hasNext()) tiles += Position.fromString(`in`.nextName()) to `in`.nextString()
+		while (`in`.hasNext()) tiles += Position.fromStringP(`in`.nextName()) to TileID.fromStringTI(`in`.nextString())
 
 		`in`.endObject()
 
